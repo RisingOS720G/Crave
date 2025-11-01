fd5177d9d839cba07411ff977b299f0c25443aa7
AbdoAhmedElbanaa <115952879+AbdoAhmedElbanaa@users.noreply.github.com>
2025-11-01 23:01:14 +0200

    Add Build.sh script for RisingOS ROM setup
    This script initializes the ROM source repository, clones necessary device-related repositories, and sets up the build environment for the RisingOS project.

---
+++ Build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Remove old local_manifests
+rm -rf .repo/local_manifests/
+
+# ROM source repo
+repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
+
+
+
+
+# Device Tree
+git clone https://github.com/RisingOS720G/device_xiaomi_miatoll.git -b 16 device/xiaomi/miatoll
+
+# Device Vendor
+git clone https://github.com/RisingOS720G/vendor_xiaomi_miatoll.git -b 16 vendor/xiaomi/miatoll
+
+# Device Kernel
+git clone https://github.com/RisingOS720G/kernel_xiaomi_sm6250.git -b 16 kernel/xiaomi/sm6250
+
+# Device Hardware 
+git clone https://github.com/RisingOS720G/hardware_xiaomi.git -b 16 hardware/xiaomi
+
+# Timekeep Hardware 
+git clone https://github.com/LineageOS/android_hardware_sony_timekeep.git -b lineage-22.2 hardware/sony/timekeep
+
+# MiuiCamera
+git clone https://github.com/RisingOS720G/vendor_xiaomi_miuicamera.git -b 16 vendor/xiaomi/miuicamera
+
+
+
+
+# Sync
+/opt/crave/resync.sh
+
+# Set up build environment
+source build/envsetup.sh
+
+# Lunch
+riseup miatoll userdebug
+
+# Rom Build 
+rise b
+
+echo -e "Rom Builded "
