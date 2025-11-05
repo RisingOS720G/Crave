e5afaf74fc898573690ec8c144d53d04f4276751
AbdoAhmedElbanaa <115952879+AbdoAhmedElbanaa@users.noreply.github.com>
2025-11-05 14:43:11 +0200

    Update repository URLs in Build.sh

--- Build.sh
+++ Build.sh
@@ -4,16 +4,18 @@
 rm -rf .repo/local_manifests/
 
 # ROM source repo
-repo init -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
+repo init -u https://github.com/RisingOS720G/android -b sixteen --git-lfs
 
 
 
 
 # Device Tree
 git clone https://github.com/RisingOS720G/device_xiaomi_miatoll.git -b 16 device/xiaomi/miatoll
+git clone https://github.com/RisingOS720G/device_xiaomi_sm6250-common.git -b 16 device/xiaomi/sm6250-common
 
 # Device Vendor
 git clone https://github.com/RisingOS720G/vendor_xiaomi_miatoll.git -b 16 vendor/xiaomi/miatoll
+git clone https://github.com/RisingOS720G/vendor_xiaomi_sm6250-common.git -b 16 vendor/xiaomi/sm6250-common
 
 # Device Kernel
 git clone https://github.com/RisingOS720G/kernel_xiaomi_sm6250.git -b 16 kernel/xiaomi/sm6250
@@ -25,7 +27,7 @@ git clone https://github.com/RisingOS720G/hardware_xiaomi.git -b 16 hardware/xia
 git clone https://github.com/LineageOS/android_hardware_sony_timekeep.git -b lineage-22.2 hardware/sony/timekeep
 
 # MiuiCamera
-git clone https://github.com/RisingOS720G/vendor_xiaomi_miuicamera.git -b 16 vendor/xiaomi/miuicamera
+git clone https://github.com/RisingOS720G/vendor_xiaomi_miuicamera-miatoll.git -b 16 vendor/xiaomi/miuicamera-miatoll
 
 
 
